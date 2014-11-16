713763940b4a8c12779db0e2538c93ed17458d45
Jonas Schwammberger <hooovercat@gmail.com>
2014-11-16 20:11:45 +0100

    did some more doc

--- project/CompressionTesting/CompressionTesting/FileWriter/StandardShortWriter.cs
+++ project/CompressionTesting/CompressionTesting/FileWriter/StandardShortWriter.cs
@@ -50,7 +50,7 @@ namespace CompressionTesting.FileWriter
             Double[] b0a = new Double[] { input.b0 };
             Double[] l0a = new Double[] { input.l0 };
             Object[][] data = new Object[1][];
-            Object[] dataRow = new Object[] { b0a, l0a, ptr, ptr_nz_len, ptph, ptth };
+            Object[] dataRow = new Object[] { b0a, l0a, ptr_nz_len, ptr, ptph, ptth };
             data[0] = dataRow;
 
             BinaryTable table = new BinaryTable(data);
@@ -59,8 +59,8 @@ namespace CompressionTesting.FileWriter
             BinaryTableHDU bhdu = (BinaryTableHDU)fits.GetHDU(1);
             bhdu.SetColumnName(0, "B0", null);
             bhdu.SetColumnName(1, "L0", null);
-            bhdu.SetColumnName(2, "PTR", null);
-            bhdu.SetColumnName(3, "PTR_NZ_LEN", null);
+            bhdu.SetColumnName(2, "PTR_NZ_LEN", null);
+            bhdu.SetColumnName(3, "PTR", null); 
             bhdu.SetColumnName(4, "PTPH", null);
             bhdu.SetColumnName(5, "PTTH", null);
             
